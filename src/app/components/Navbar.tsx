4865b7c77f69a292ea216326aed03370cfd0d8ae
Zahra Hozhabri <zahra.hjri7272@gmail.com>
2025-03-10 05:06:38 +0330

    added new public file

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -9,17 +9,23 @@ import { useCart } from "@/context/CartContext";
 const Navbar = () => {
   const { handleOpenCart, isOpenCart } = useCart();
   return (
-    <nav className=" flex gap-10 min-h-[80px] px-20 py-8 justify-between sticky top-0 z-20 bg-white-100 shadow-2xl">
+    <nav className=" flex gap-10 min-h-[80px] px-20 py-8 justify-between sticky top-0 z-20 bg-white-100 shadow-xl">
       <h2 className="text-4xl font-normal">miniture</h2>
       <div className="flex gap-5 items-center">
         <div className="relative cursor-pointer" onClick={handleOpenCart}>
           <div className="w-[25px] h-[25px] text-white rounded-full absolute bg-orange-500 flex items-center justify-center -top-2 -right-2">
             0
           </div>
-          <CiShoppingCart size={35} />
+          <div>
+            <CiShoppingCart size={35} />
+          </div>
         </div>
-        <IoPersonOutline size={30} />
-        <GrFavorite size={30} />
+        <div className="cursor-pointer">
+          <IoPersonOutline size={30} />
+        </div>
+        <div className="cursor-pointer">
+          <GrFavorite size={30} />
+        </div>
       </div>
 
       {isOpenCart && (
